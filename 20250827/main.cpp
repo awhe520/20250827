39ec377e9177848b11d4d8ef821f2fc23db0ae07
Marcus <awhe520@gmail.com>
2025-08-27 23:12:41 +0800

    Initial Commit

---
+++ 20250827/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  20250827
+//
+//  Created by 張舜俊 on 2025/8/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
